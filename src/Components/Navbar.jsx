fad2e84e5fbe8c77650dbd879471d1b85773a3f8
ken314526 <abhisheksharma801831@gmail.com>
2023-08-28 21:29:51 +0530

    Updating FIles

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,11 +13,11 @@ const Navbar = () => {
     console.log(isopen);
   };
 
-  let login = false;
+  let login = true;
 
   return (
     <>
-      <div className="container-xxl navbar px-5 sticky top-0 w-full  items-center justify-between pt-2 z-50 flex flex-wrap bg-white shadow-md">
+      <div className="navbar px-5 sticky top-0 w-full  items-center justify-between pt-2 z-50 flex flex-wrap bg-white shadow-md">
         {/* left portion of navbar  */}
         <div className="left border-1  w-auto md:w-1/5 ">
           <img src={logo} alt="logo" className="h-9 hover:cursor-pointer"></img>
